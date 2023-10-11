8c09b9b9f52ddf1a4b85d7f063cb85684b7419e2
Shishant21 <122087453+Shishant21@users.noreply.github.com>
2023-10-11 15:56:16 +0530

    Update trailcheck.py

--- trailcheck.py
+++ trailcheck.py
@@ -7,12 +7,13 @@
 # # df = pd.read_csv(github_csv_url)
 # st.write("Trail test 1")
 
-import streamlit as st
-import numpy as np
-import pandas as pd
 # import matplotlib as plt
 # import plotly.express as px
 # from util import same_timestamp
+import streamlit as st
+import numpy as np
+import pandas as pd
+
 
 # Streamlit Page Config
 st.set_page_config(page_title="Superstore!!!", page_icon=":bar_chart:",layout="wide")
