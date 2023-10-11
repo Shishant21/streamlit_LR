8c3e4a6c950d3a17117d28084c0c18a9cad37f42
Shishant21 <122087453+Shishant21@users.noreply.github.com>
2023-10-11 15:45:00 +0530

    Update trailcheck.py

--- trailcheck.py
+++ trailcheck.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 # df= pd.read_csv(r"Superstore.csv", header=0,encoding='latin-1')
 # st.write(df)
 github_csv_url="https://github.com/Shishant21/streamlit_LR/blob/main/Superstore.csv"
