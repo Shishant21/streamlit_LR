076a9f5acf3bcd3a1a74fb2ec8b8d79f5ae58750
Shishant21 <122087453+Shishant21@users.noreply.github.com>
2023-10-23 12:24:02 +0530

    Update trailcheck.py

--- trailcheck.py
+++ trailcheck.py
@@ -99,11 +99,11 @@ with tab2:
       st.download_button('Download Data', data = csv, file_name = "Segment.csv", mime ='text/csv')
 
   st.subheader('Sub-Category wise Sales')
-  figbar = px.bar(Sub_df, x='Sub-Category', y='Sales')
+  figbar = px.bar(sub_df, x='Sub-Category', y='Sales')
   st.plotly_chart(figbar,use_container_width=True)
   with st.expander("Sub-Category wise Sales:"):
-      st.write(Sub_df)
-      csv = Sub_df.to_csv(index=False).encode("utf-8")
+      st.write(sub_df)
+      csv = sub_df.to_csv(index=False).encode("utf-8")
       st.download_button('Download Data', data = csv, file_name = "Sub_Category.csv", mime ='text/csv')
   
   
