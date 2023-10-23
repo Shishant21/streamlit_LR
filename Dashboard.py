d233d6d5bff0e0be85145570add6fe842d0d1d2e
Shishant21 <122087453+Shishant21@users.noreply.github.com>
2023-10-23 11:25:48 +0530

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -24,80 +24,79 @@ date1 = pd.to_datetime(st.sidebar.date_input("Start Date", startDate))
 
 date2 = pd.to_datetime(st.sidebar.date_input("End Date", endDate))
 tab1, tab2 = st.tabs(["Data", "Charts"])
+
+df = df[(df["Order Date"] >= date1) & (df["Order Date"] <= date2)].copy()
+
+st.sidebar.header("Choose your filter: ")
+# Create for Region
+region = st.sidebar.multiselect("Pick your Region", df["Region"].unique())
+if not region:
+    df2 = df.copy()
+else:
+    df2 = df[df["Region"].isin(region)]
+
+# Create for State
+state = st.sidebar.multiselect("Pick the State", df2["State"].unique())
+if not state:
+    df3 = df2.copy()
+else:
+    df3 = df2[df2["State"].isin(state)]
+
+# Create for City
+city = st.sidebar.multiselect("Pick the City",df3["City"].unique())
+
+# Filter the data based on Region, State and City
+
+if not region and not state and not city:
+    filtered_df = df
+elif not state and not city:
+    filtered_df = df[df["Region"].isin(region)]
+elif not region and not city:
+    filtered_df = df[df["State"].isin(state)]
+elif state and city:
+    filtered_df = df3[df["State"].isin(state) & df3["City"].isin(city)]
+elif region and city:
+    filtered_df = df3[df["Region"].isin(region) & df3["City"].isin(city)]
+elif region and state:
+    filtered_df = df3[df["Region"].isin(region) & df3["State"].isin(state)]
+elif city:
+    filtered_df = df3[df3["City"].isin(city)]
+else:
+    filtered_df = df3[df3["Region"].isin(region) & df3["State"].isin(state) & df3["City"].isin(city)]
+
+category_df = filtered_df.groupby(by = ["Category"], as_index = False)["Sales"].sum()
+
 with tab1:
-    df = df[(df["Order Date"] >= date1) & (df["Order Date"] <= date2)].copy()
-    
-    st.sidebar.header("Choose your filter: ")
-    # Create for Region
-    region = st.sidebar.multiselect("Pick your Region", df["Region"].unique())
-    if not region:
-        df2 = df.copy()
-    else:
-        df2 = df[df["Region"].isin(region)]
-    
-    # Create for State
-    state = st.sidebar.multiselect("Pick the State", df2["State"].unique())
-    if not state:
-        df3 = df2.copy()
-    else:
-        df3 = df2[df2["State"].isin(state)]
-    
-    # Create for City
-    city = st.sidebar.multiselect("Pick the City",df3["City"].unique())
-    
-    # Filter the data based on Region, State and City
-    
-    if not region and not state and not city:
-        filtered_df = df
-    elif not state and not city:
-        filtered_df = df[df["Region"].isin(region)]
-    elif not region and not city:
-        filtered_df = df[df["State"].isin(state)]
-    elif state and city:
-        filtered_df = df3[df["State"].isin(state) & df3["City"].isin(city)]
-    elif region and city:
-        filtered_df = df3[df["Region"].isin(region) & df3["City"].isin(city)]
-    elif region and state:
-        filtered_df = df3[df["Region"].isin(region) & df3["State"].isin(state)]
-    elif city:
-        filtered_df = df3[df3["City"].isin(city)]
-    else:
-        filtered_df = df3[df3["Region"].isin(region) & df3["State"].isin(state) & df3["City"].isin(city)]
-    
-    category_df = filtered_df.groupby(by = ["Category"], as_index = False)["Sales"].sum()
-    
-    
-# with tab1:
     st.write(category_df)
 
 with tab2:
-    with col1:
-        st.subheader("Category wise Sales")
-        fig = px.bar(category_df, x = "Category", y = "Sales", text = ['${:,.2f}'.format(float(x)) for x in category_df["Sales"]],
-                     template = "seaborn")
-        st.plotly_chart(fig,use_container_width=True, height = 200)
+    # with col1:
+    #     st.subheader("Category wise Sales")
+    #     fig = px.bar(category_df, x = "Category", y = "Sales", text = ['${:,.2f}'.format(float(x)) for x in category_df["Sales"]],
+    #                  template = "seaborn")
+    #     st.plotly_chart(fig,use_container_width=True, height = 200)
     
-    with col2:
-        st.subheader("Region wise Sales")
-        fig = px.pie(filtered_df, values = "Sales", names = "Region", hole = 0.5)
-        fig.update_traces(text = filtered_df["Region"], textposition = "outside")
-        st.plotly_chart(fig,use_container_width=True)
+    # with col2:
+    #     st.subheader("Region wise Sales")
+    #     fig = px.pie(filtered_df, values = "Sales", names = "Region", hole = 0.5)
+    #     fig.update_traces(text = filtered_df["Region"], textposition = "outside")
+    #     st.plotly_chart(fig,use_container_width=True)
     
-    cl1, cl2 = st.columns((2))
-    with cl1:
-        with st.expander("Category_ViewData"):
-            st.write(category_df.style.background_gradient(cmap="Blues"))
-            csv = category_df.to_csv(index = False).encode('utf-8')
-            st.download_button("Download Data", data = csv, file_name = "Category.csv", mime = "text/csv",
-                                help = 'Click here to download the data as a CSV file')
+    # cl1, cl2 = st.columns((2))
+    # with cl1:
+    #     with st.expander("Category_ViewData"):
+    #         st.write(category_df.style.background_gradient(cmap="Blues"))
+    #         csv = category_df.to_csv(index = False).encode('utf-8')
+    #         st.download_button("Download Data", data = csv, file_name = "Category.csv", mime = "text/csv",
+    #                             help = 'Click here to download the data as a CSV file')
     
-    with cl2:
-        with st.expander("Region_ViewData"):
-            region = filtered_df.groupby(by = "Region", as_index = False)["Sales"].sum()
-            st.write(region.style.background_gradient(cmap="Oranges"))
-            csv = region.to_csv(index = False).encode('utf-8')
-            st.download_button("Download Data", data = csv, file_name = "Region.csv", mime = "text/csv",
-                            help = 'Click here to download the data as a CSV file')
+    # with cl2:
+    #     with st.expander("Region_ViewData"):
+    #         region = filtered_df.groupby(by = "Region", as_index = False)["Sales"].sum()
+    #         st.write(region.style.background_gradient(cmap="Oranges"))
+    #         csv = region.to_csv(index = False).encode('utf-8')
+    #         st.download_button("Download Data", data = csv, file_name = "Region.csv", mime = "text/csv",
+    #                         help = 'Click here to download the data as a CSV file')
             
     filtered_df["month_year"] = filtered_df["Order Date"].dt.to_period("M")
     st.subheader('Time Series Analysis')
